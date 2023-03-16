1169bd6960faf049d9a5c52fd902d4dee32adfa2
fs000 <flaviosantos002010@hotmail.com>
2023-03-16 16:14:42 +0000

    Add variable that controls terrain setup and modify one other name

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -8,12 +8,16 @@ namespace LandscaperAnts {
     [Serializable]
     public sealed class Grid {
 
-        [Header("Nodes Generation Settings")]
+        [Header("Grid Generation Settings")]
+
+        [SerializeField]
+        [Tooltip("Controls wether the terrain will start as a float surface or with Perlin Noise applied")]
+        private bool flatTerrain = true;
 
         [SerializeField]
         [Range(2, 100)]
-        [Tooltip("The amount of nodes to generate")]
-        private int nodesAmount = 4;
+        [Tooltip("The amount of food to generate")]
+        private int foodAmount = 4;
 
         [SerializeField]
         [Tooltip("The dimensions of the grid")]
@@ -55,9 +59,9 @@ namespace LandscaperAnts {
         // Create a collection of points on random positions
         private void CreateNodes() {
 
-            foodCells = new Vector2Int[nodesAmount];
+            foodCells = new Vector2Int[foodAmount];
 
-            for (int i = 0; i < nodesAmount; i++) {
+            for (int i = 0; i < foodAmount; i++) {
 
                 int x = Random.Range(0, baseDim);
                 int y = Random.Range(0, baseDim);
@@ -77,11 +81,17 @@ namespace LandscaperAnts {
 
                 for (int j = 0; j < baseDim; j++) {
 
-                    // Set inital height
-                    //heights[i, j] = 1.0f;
-                    heights[i, j] = Mathf.PerlinNoise(
-                        (10f * i) / baseDim,
-                        (10f * j) / baseDim);
+                    // Set initial height
+                    if (flatTerrain) {
+
+                        heights[i, j] = 1.0f;
+
+                    } else {
+
+                        heights[i, j] = Mathf.PerlinNoise(
+                            (10f * i) / baseDim,
+                            (10f * j) / baseDim);
+                    }
 
                     // Set initial pheromone amount
                     pheromones[i, j] = initPheromones;
@@ -93,10 +103,10 @@ namespace LandscaperAnts {
         public int[] GetNeighbours(int node) {
 
             // Neighbours are all points which don't correspond to the node itself
-            int[] neighbours = new int[nodesAmount - 1];
+            int[] neighbours = new int[foodAmount - 1];
 
             // Find neighbours of 'node' based on index
-            for (int i = 0; i < nodesAmount; i++) {
+            for (int i = 0; i < foodAmount; i++) {
 
                 // node = "from" | i = "to"
                 if (node != i) {
@@ -128,7 +138,7 @@ namespace LandscaperAnts {
                 return;
 
             // Instantiate each sprite
-            for (int i = 0; i < nodesAmount; i++) {
+            for (int i = 0; i < foodAmount; i++) {
 
                 Vector3 spritePos = TexelToVector(foodCells[i]);
 

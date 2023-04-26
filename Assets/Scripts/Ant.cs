2474a75389ff0cc708f7d758c7181a2cf37c1118
flavio <flaviosantos002010@hotmail.com>
2023-04-26 22:30:33 +0100

    Alter an Ant's possession of food and add a method to deposit pheromones according to the path being taken

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -4,7 +4,7 @@ namespace LandscaperAnts
 {
     public sealed class Ant
     {
-        public bool HasFood { get; set; }
+        public Food Food { get; set; }
 
         public Vector2Int ColonyCell { get; private set; }
 
@@ -25,5 +25,25 @@ namespace LandscaperAnts
 
         // Check if the Ant is (back) at home
         public bool IsHome() => CurrentCell == ColonyCell;
+
+        public bool HasFood() => Food is not null;
+
+        // Function -> https://www.desmos.com/calculator/uszs8torq5
+        public float DropPheromone(float deposit, float minPercentage = 0f)
+        {
+            float totalDistance = Vector2.Distance(Food.Cell, ColonyCell);
+
+            // Prevent division by 0
+            if (Mathf.Approximately(totalDistance, 0f))
+            {
+                return deposit;
+            }
+
+            float currentDistanceToHome = Vector2.Distance(CurrentCell, ColonyCell);
+
+            float minDepositPercentage = deposit * minPercentage;
+
+            return ((currentDistanceToHome * (deposit - minDepositPercentage)) / totalDistance) + minDepositPercentage;
+        }
     }
 }
